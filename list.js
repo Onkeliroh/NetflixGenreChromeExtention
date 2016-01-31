ffb3dc0cbb1a1488ac231107caa8b2f9dfbe0c2d
onkeliroh <onkeliroh@gmx.de>
2016-01-31 17:38:14 +0100

    began genre filter

--- list.js
+++ list.js
@@ -1,8 +1,29 @@
 // quasi main
-document.addEventListener("DOMContentLoaded",function(){
-  createList();
-  attachEvents();
-});
+document.addEventListener("DOMContentLoaded",
+  main);
+
+function main(){
+  createList("All");
+
+  attachMetaGenres();
+
+  document.getElementById("FilterSelect").addEventListener("change",filterChanged);
+}
+
+function filterChanged(){
+  selector = document.getElementById("FilterSelect");
+
+  option = getSelectedOption(selector);
+
+  createList(option);
+}
+
+function getSelectedOption(selectElement){
+  if (selectElement.selectedIndex == -1)
+    return null;
+
+  return selectElement.options[selectElement.selectedIndex].text;
+}
 
 function attachEvents(){
   $("p.categorie").mousedown(function(){
@@ -23,13 +44,48 @@ function getIndex(str){
   return -1;
 }
 
-function createList(){
-  for (i = 0; i < categories.length; i++)
-  {
-    appendCategorie(categories[i][1]);
+function createList(filter){
+  //gain access to element
+  var genrecontainer = document.getElementById("CategorieListContainer");
+
+  //remove old List
+  while(genrecontainer.hasChildNodes()){
+    genrecontainer.removeChild(genrecontainer.lastChild);
   }
+
+  if (filter == "All" || filter == "" )
+  {
+    for (i = 0; i < categories.length; i++)
+    {
+      appendGenre(categories[i][1]);
+    }
+  }
+  else {
+    for (i = 0; i < categories.length; i++)
+    {
+      if (strContains(categories[i][1],filter))
+      {
+        appendGenre(categories[i][1]);
+      }
+    }
+  }
+  attachEvents();
 }
 
-function appendCategorie(CategorieText) {
-  $("#CategorieListContainer").append("<p class='categorie' style='font-weight:bold; color:#fff; cursor:pointer;'>" + CategorieText + "</p>");
+function appendGenre(genre){
+  $("#CategorieListContainer").append("<p class='categorie'>" + genre  + "</p>");
+}
+
+function strContains(s1,s2){
+  return (s1.indexOf(s2) > -1);
+}
+
+function attachMetaGenres(){
+  for (i = 0; i< metagenres.length; i++)
+  {
+    var option = document.createElement("option");
+    option.text = metagenres[i];
+
+    document.getElementById("FilterSelect").add(option);
+  }
 }
